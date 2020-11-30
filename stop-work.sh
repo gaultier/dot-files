cafba962f2e6f4c0f5ae752b1d9276e49f16c98c
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-11-30 17:02:40 +0100

    Mon Nov 30 17:03:32 CET 2020

--- stop-work.sh
+++ stop-work.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 pgrep -i 'Mail|Calendar|k9s|skaffold|kubectl|viscosity|java|intellij|idea|docker|chat|openvpn' | xargs kill -9
-su pgaultier
-blueutil -p off
+BLUEUTIL_ALLOW_ROOT=1 blueutil -p off
 
