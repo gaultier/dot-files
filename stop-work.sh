71fa5ffc054bbf688e99ca94d74e16eb336d7899
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-12-15 17:23:51 +0100

    kill vim gracefully & allow kitty remote control

--- stop-work.sh
+++ stop-work.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-pgrep -i 'Mail|Calendar|k9s|skaffold|kubectl|viscosity|java|intellij|idea|docker|chat|openvpn|redis|git|less|man|clangd|node|gopls|nvim' | xargs kill -9
+pkill nvim
+pgrep -i 'Mail|Calendar|k9s|skaffold|kubectl|viscosity|java|intellij|idea|docker|chat|openvpn|redis|git|less|man|clangd|node|gopls' | xargs kill -9
 BLUEUTIL_ALLOW_ROOT=1 blueutil -p off
 
