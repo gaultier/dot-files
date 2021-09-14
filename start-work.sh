46a175b946ed59ebd5eae0e6fc9fc7f6828fe357
Philippe Gaultier <philippe.gaultier@ppro.com>
2021-09-14 16:48:43 +0200

    start-work

---
+++ start-work.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -x
+
+nvim -R -c ':PlugUpdate' -c ':PlugClean' -c ':qa'
+
+if which brew >/dev/null 2>&1; then
+ brew update
+ brew upgrade
+ brew cleanup
+fi
+
+if which pkgin >/dev/null 2>/dev/null; then
+  sudo pkgin update
+  sudo pkgin upgrade
+fi
+
+if which pacman >/dev/null 2>/dev/null; then
+  sudo pacman -Syu
+fi
