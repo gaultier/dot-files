5ce4259419e71486f6c29426a25d8508817d527a
Philippe Gaultier <philippe.gaultier@ppro.com>
2021-10-13 16:16:01 +0200

    kill more

--- stop-work.sh
+++ stop-work.sh
@@ -3,7 +3,7 @@
 # Kill gracefully curses programs that leave a mess in the terminal if killed forcefully
 pkill nvim k9s
 # Kill forcefully the remaining processes
-pgrep -i 'safari|chromium|emacs|Mail|Calendar|k9s|skaffold|kubectl|viscosity|java|intellij|idea|docker|chat|openvpn|redis|git|less|man|clangd|node|gopls|top|htop|find|mysql|mariadb|ruby|python|php|nginx|perl|vim|virtualbox|vbox' | xargs kill -9
+pgrep -i 'safari|chromium|emacs|Mail|Calendar|k9s|skaffold|kubectl|viscosity|java|intellij|idea|docker|chat|openvpn|redis|git|less|man|clangd|node|gopls|top|htop|find|mysql|mariadb|ruby|python|php|nginx|perl|vim|virtualbox|vbox|kafka|zookeeper|ssh|go' | xargs kill -9
 BLUEUTIL_ALLOW_ROOT=1 blueutil -p off
 
 brew services stop --all
