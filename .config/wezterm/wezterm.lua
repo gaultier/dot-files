81af3c66044f303afd25747c443e33fdcd95ccce
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-01-18 10:09:25 +0100

    increase scrollback & fmt

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,10 +3,11 @@ local wezterm = require 'wezterm';
 return {
   color_scheme = "ChallengerDeep",
   font = wezterm.font_with_fallback({
-          "SauceCodePro Nerd Font",
-          "JetBrains Mono",
-  }),
+      "SauceCodePro Nerd Font",
+      "JetBrains Mono",
+    }),
   font_size = 12.0,
+  scrollback_lines = 10000,
   keys = {
     {key=".", mods="ALT", action=wezterm.action{ActivateTabRelative=1}},
     {key=",", mods="ALT", action=wezterm.action{ActivateTabRelative=-1}},
