930cf3c8ae1a4199d206552c68ed82a15efedace
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-03-31 09:34:04 +0200

    gitlab url + visual

--- .config/nvim/lua/gitlab.lua
+++ .config/nvim/lua/gitlab.lua
@@ -12,16 +12,27 @@ function get_gitlab_project_path()
 end
 
 function get_git_current_branch()
-  return exec_and_get_output_cmd('git rev-parse --abbrev-ref HEAD')
+  return string.sub(exec_and_get_output_cmd('git rev-parse --abbrev-ref HEAD'), 0, -2) -- Trim newline
 end
 
 function get_current_line()
   return vim.api.nvim_win_get_cursor(0)[1]
 end
 
-function gitlab_url_at_point(line)
+function get_visual_range_lines()
+    local start_y = vim.api.nvim_buf_get_mark(0, '<')[1]
+    local end_y = vim.api.nvim_buf_get_mark(0, '>')[1]
+    return start_y, end_y
+end
+
+function gitlab_url_at_point(start_y, end_y)
   local file_path = vim.fn.expand('%')
-  return 'https://gitlab.ppro.com/' .. get_gitlab_project_path() .. '/-/tree/' .. get_git_current_branch() .. '/' .. file_path .. '#L' .. line
+  local url = 'https://gitlab.ppro.com/' .. get_gitlab_project_path() .. '/-/tree/' .. get_git_current_branch() .. '/' .. file_path .. '#L' .. start_y
+  if end_y then
+      url = url .. '-' .. end_y
+  end
+
+  return url
 end
 
 function open_gitlab_url_at_point()
@@ -30,6 +41,16 @@ end
 
 function copy_gitlab_url_at_point()
   vim.fn.setreg('*', gitlab_url_at_point(get_current_line()))
+end
+
+function copy_gitlab_url_visual_range()
+  local start_y, end_y = get_visual_range_lines()
+  vim.fn.setreg('*', gitlab_url_at_point(start_y, end_y))
+end
+
+function open_gitlab_url_visual_range()
+  local start_y, end_y = get_visual_range_lines()
+  os.execute('open "' .. gitlab_url_at_point(start_y, end_y) .. '"')
 end
 
 -- vim.api.nvim_add_user_command('GitlabUrlAtPointOpen', open_gitlab_url_at_point)
