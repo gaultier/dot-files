bed03487fd271987f67edd40fe8a5191530cc5a9
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-04-19 15:21:03 +0200

    add cmd arg

--- .config/nvim/lua/gitlab.lua
+++ .config/nvim/lua/gitlab.lua
@@ -19,12 +19,15 @@ function get_path_from_git_root()
   return string.sub(exec_and_get_output_cmd('git rev-parse --show-prefix'), 0, -2) -- Trim newline
 end
 
-function gitlab_url_at_point(start_y, end_y)
+function gitlab_url_at_point(start_y, end_y, with_line_number)
   local path_from_git_root = get_path_from_git_root()
   local file_path = vim.fn.expand('%')
-  local url = 'https://gitlab.ppro.com/' .. get_gitlab_project_path() .. '/-/tree/' .. get_git_current_branch() .. '/' .. path_from_git_root .. file_path .. '#L' .. start_y
-  if end_y then
-      url = url .. '-' .. end_y
+  local url = 'https://gitlab.ppro.com/' .. get_gitlab_project_path() .. '/-/tree/' .. get_git_current_branch() .. '/' .. path_from_git_root .. file_path
+  if with_line_number == "true" then
+    url = url .. '#L' .. start_y
+    if end_y then
+        url = url .. '-' .. end_y
+    end
   end
 
   return url
@@ -35,12 +38,12 @@ function copy_gitlab_url(cmd)
   if cmd.reg == '' then 
     reg = '*' 
   end
-  vim.fn.setreg(reg, gitlab_url_at_point(cmd.line1, cmd.line2))
+  vim.fn.setreg(reg, gitlab_url_at_point(cmd.line1, cmd.line2, cmd.fargs[1]))
 end
 
 function open_gitlab_url(cmd)
-  os.execute('open "' .. gitlab_url_at_point(cmd.line1, cmd.line2) .. '"')
+  os.execute('open "' .. gitlab_url_at_point(cmd.line1, cmd.line2, cmd.fargs[1]) .. '"')
 end
 
-vim.api.nvim_add_user_command('GitlabUrlCopy', copy_gitlab_url, {range=true})
-vim.api.nvim_add_user_command('GitlabUrlOpen', open_gitlab_url, {range=true})
+vim.api.nvim_add_user_command('GitlabUrlCopy', copy_gitlab_url, {range=true, nargs=1})
+vim.api.nvim_add_user_command('GitlabUrlOpen', open_gitlab_url, {range=true, nargs=1})
