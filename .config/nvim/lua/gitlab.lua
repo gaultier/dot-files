af1732ace12b51b543917bc917f0dd79d57e422b
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-04-25 13:39:18 +0200

    add GitlabUrlxxxMaster

--- .config/nvim/lua/gitlab.lua
+++ .config/nvim/lua/gitlab.lua
@@ -19,10 +19,14 @@ function get_path_from_git_root()
   return string.sub(exec_and_get_output_cmd('git rev-parse --show-prefix'), 0, -2) -- Trim newline
 end
 
-function gitlab_url_at_point(start_y, end_y, with_line_number)
+function gitlab_url_at_point(start_y, end_y, with_line_number, branch)
   local path_from_git_root = get_path_from_git_root()
   local file_path = vim.fn.expand('%')
-  local url = 'https://gitlab.ppro.com/' .. get_gitlab_project_path() .. '/-/tree/' .. get_git_current_branch() .. '/' .. path_from_git_root .. file_path
+  if not branch then
+    branch = get_git_current_branch()
+  end
+
+  local url = 'https://gitlab.ppro.com/' .. get_gitlab_project_path() .. '/-/tree/' .. branch .. '/' .. path_from_git_root .. file_path
   if with_line_number == "true" then
     url = url .. '#L' .. start_y
     if end_y then
@@ -45,5 +49,20 @@ function open_gitlab_url(cmd)
   os.execute('open "' .. gitlab_url_at_point(cmd.line1, cmd.line2, cmd.fargs[1]) .. '"')
 end
 
+
+function copy_gitlab_url_master(cmd)
+  local reg = cmd.reg
+  if cmd.reg == '' then 
+    reg = '*' 
+  end
+  vim.fn.setreg(reg, gitlab_url_at_point(cmd.line1, cmd.line2, cmd.fargs[1], 'master'))
+end
+
+function open_gitlab_url_master(cmd)
+  os.execute('open "' .. gitlab_url_at_point(cmd.line1, cmd.line2, cmd.fargs[1], 'master') .. '"')
+end
+
 vim.api.nvim_create_user_command('GitlabUrlCopy', copy_gitlab_url, {range=true, nargs=1})
 vim.api.nvim_create_user_command('GitlabUrlOpen', open_gitlab_url, {range=true, nargs=1})
+vim.api.nvim_create_user_command('GitlabUrlCopyMaster', copy_gitlab_url_master, {range=true, nargs=1})
+vim.api.nvim_create_user_command('GitlabUrlOpenMaster', open_gitlab_url_master, {range=true, nargs=1})
