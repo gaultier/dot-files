fe65faed9c6f2712813b6a61ca46587cf5d67996
Philippe Gaultier <philippe.gaultier@gi-de.com>
2023-05-25 17:17:06 +0200

    polybar on each monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,10 +6,8 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-polybar
-# Launch bar1 and bar2
-# echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-# polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-# polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+  MONITOR=$m polybar --reload &
+done
 
 echo "Bars launched..."
