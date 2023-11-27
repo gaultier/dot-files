222f21d65de65db782ccfc4bf81d9cd6ad128dc9
Philippe Gaultier <philippe.gaultier@gi-de.com>
2023-11-27 13:03:11 +0100

    low battery warning

--- bar.sh
+++ bar.sh
@@ -2,6 +2,13 @@
 
 battery_info="$(upower -i /org/freedesktop/UPower/devices/battery_BAT0)"
 percentage="$(echo "$battery_info" | awk '/percentage:/ {print $2}')"
+
+battery_is_low=`test "$(echo "$battery_info" | awk '/percentage:/ {print $2+0}')" -lt 10`
+if ! $battery_is_low; then
+  notify-send -u critical --icon battery -t 5000 'Low Battery!'
+fi
+
+
 state="$(echo "$battery_info" | awk '/state:/ {print $2}')"
 time_to_full="$(echo "$battery_info" | awk '/time to full:/ {for (i=4; i<NF; i++) printf $i " "; print $NF}')"
 now="$(date)"
