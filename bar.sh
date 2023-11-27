40167ff9c9d87234cd501aad9516c2aea5d659da
Philippe Gaultier <philippe.gaultier@gi-de.com>
2023-11-27 12:51:34 +0100

    show more in status bar

--- bar.sh
+++ bar.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-percentage="$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/percentage:/ {print $2}')"
-state="$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/state:/ {print $2}')"
-time_to_full="$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/time to full:/ {print $4, $5}')"
+battery_info="$(upower -i /org/freedesktop/UPower/devices/battery_BAT0)"
+percentage="$(echo "$battery_info" | awk '/percentage:/ {print $2}')"
+state="$(echo "$battery_info" | awk '/state:/ {print $2}')"
+time_to_full="$(echo "$battery_info" | awk '/time to full:/ {for (i=4; i<NF; i++) printf $i " "; print $NF}')"
 now="$(date)"
 
-printf "Battery: %s %s %s | %s" "$percentage" "$state" "$time_to_full" "$now"
+wifi="$(nmcli --terse --fields in-use,ssid,bars device wifi list | awk -F ':' '$1=="*" {print $2, $3}')"
+
+bluetooth="$(bluetoothctl devices)"
+
+ethernet="$(nmcli -t -g general.state device show enx00e04c709655)"
+
+printf "Ethernet: %s | Bluetooth: %s | Wifi: %s | Battery: %s %s %s | %s" "$ethernet" "$bluetooth" "$wifi" "$percentage" "$state" "$time_to_full" "$now"
