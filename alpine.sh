f33942716f970459ff79510e2df271ead0d5604e
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-06-10 08:10:18 +0200

    alpine

--- alpine.sh
+++ alpine.sh
@@ -1,3 +1,13 @@
-apk add sway swaylock swayidle neovim curl ripgrep linux-firmware fish kitty jq git clang
+doas apk add sway swaylock swayidle neovim curl ripgrep linux-firmware fish kitty jq git mpv networkmanager iwd dbus mandoc fzf fd build-base
 
-setup-desktop gnome
+doas setup-desktop gnome
+doas chsh -s $(which fish) $(whoami)
+
+# Wifi
+doas setup-devd udev
+doas rc-service iwd start
+doas rc-update iwd default
+doas rc-service networkmanager start
+doas sh -c 'echo "[device]\nwifi.backend=iwd" > /etc/NetworkManager/NetworkManager.conf'
+doas rc-update add networkmanager default
+doas adduser $(whoami) plugdev
