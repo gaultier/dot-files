2e8b4f8cd137aa7a84a44da583bcb440d2ea2aaf
Philippe Gaultier <philippe.gaultier@ory.sh>
2025-05-28 11:30:00 +0200

    chore(nvim): enable ts lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -227,6 +227,7 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local lspconfig = require('lspconfig')
 
+lspconfig.ts_ls.setup{}
 lspconfig.clangd.setup{}
 lspconfig.zls.setup{}
 lspconfig.gopls.setup({
