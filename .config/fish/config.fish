137cdb14c1a5c8efbd01ebd10e292b2aad933b40
Philippe Gaultier <philippe.gaultier@ory.sh>
2025-08-29 10:10:05 +0200

    brewfile: trim deps

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -92,3 +92,6 @@ end
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/opt/homebrew/share/google-cloud-sdk/path.fish.inc' ]; . '/opt/homebrew/share/google-cloud-sdk/path.fish.inc'; end
